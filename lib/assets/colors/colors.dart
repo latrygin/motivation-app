9e8b2a605dec558726c29646b391e90c2552c0ed
latrygin <latrygin@icloud.com>
2022-09-30 00:42:23 +0700

    indicate the approximate colors of the project

---
+++ lib/assets/colors/colors.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+/*
+ * Colors for light theme:
+ *  - White color
+ *  - Black color
+ *  - Light grey color (grey.shade200)
+ *  - Medium grey color (grey.shade300)
+ *  - Dark grey color (grey.shade400)
+ */
+
+const whiteColorForLightTheme = Colors.white;
+const blackColorForLightTheme = Colors.black;
+const greyLightColorForLightTheme = Color(0xFFEEEEEE);
+const greyMediumColorForLightTheme = Color(0xFF9E9E9E);
+const greyDarkColorForLightTheme = Color(0xFF9E9E9E);
+
+/*
+ * Colors for dark theme:
+ *   - White color
+ *   - Black color
+ *   - Light grey color (grey.shade700)
+ *   - Medium grey color (grey.shade800)
+ *   - Dark grey color (grey.shade900)
+ */
+
+const whiteColorForDarkTheme = Colors.white;
+const blackColorForDarkTheme = Colors.black;
+const greyLightColorForDarkTheme = Color(0xFF616161);
+const greyMediumColorForDarkTheme = Color(0xFF424242);
+const greyDarkColorForDarkTheme = Color(0xFF212121);
