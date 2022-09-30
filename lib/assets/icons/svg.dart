efe8cef9b0b94aa4017587fcc1c2d6fe0c930812
latrygin <latrygin@icloud.com>
2022-10-01 00:15:14 +0700

    Add custom SVG class
    Create custom icon class for my svg image

---
+++ lib/assets/icons/svg.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SVG extends StatelessWidget {
+  final Widget? icon;
+  final double? size;
+  final VoidCallback? onPressed;
+
+  const SVG(this.icon, {super.key, this.size, this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPressed,
+      borderRadius: BorderRadius.circular(size ?? 24),
+      child: SizedBox(
+        height: size,
+        width: size,
+        child: FittedBox(
+          fit: BoxFit.fill,
+          alignment: Alignment.center,
+          child: icon,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/assets/icons/svgs.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SVGs {
+  SVGs._();
+
+  /// <img src="/Users/latrygin/Development/motivation/assets/icons/add.svg" alt="add.svg" height=100 width=100> custom icon "Add".
+  static SvgPicture add = SvgPicture.asset(
+    'assets/icons/add.svg',
+  );
+
+  /// <img src="/Users/latrygin/Development/motivation/assets/icons/arrow_bottom_1.svg" alt="arrow_bottom_1.svg" height=100 width=100> custom icon "Arrow Botton 1".
+  static SvgPicture arrow_bottom_1 =
+      SvgPicture.asset('assets/icons/arrow_bottom_1.svg');
+
+  /// <img src="/Users/latrygin/Development/motivation/assets/icons/arrow_bottom_2.svg" alt="arrow_bottom_2.svg" height=100 width=100> custom icon "Arrow Botton 2".
+  static SvgPicture arrow_bottom_2 =
+      SvgPicture.asset('assets/icons/arrow_bottom_2.svg');
+}
