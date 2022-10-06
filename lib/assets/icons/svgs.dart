0ded1f591e3a611639760686d5ab169d227a00b4
latrygin <latrygin@icloud.com>
2022-10-06 12:05:55 +0700

    add icons in class

--- lib/assets/icons/svgs.dart
+++ lib/assets/icons/svgs.dart
@@ -5,16 +5,27 @@ import 'package:flutter_svg/flutter_svg.dart';
 class SVGs {
   SVGs._();
 
-  /// <img src="/Users/latrygin/Development/motivation/assets/icons/add.svg" alt="add.svg" height=100 width=100> custom icon "Add".
-  static SvgPicture add = SvgPicture.asset(
-    'assets/icons/add.svg',
-  );
+  /// <img src="/Users/latrygin/Development/motivation/assets/icons/icons_dark_theme/twitter-svgrepo-com.svg" alt="arrow_bottom_2.svg" height=100 width=100> custom icon "Twitter".
+  static SvgPicture twitter =
+      SvgPicture.asset('assets/icons/icons_dark_theme/twitter-svgrepo-com.svg');
 
-  /// <img src="/Users/latrygin/Development/motivation/assets/icons/arrow_bottom_1.svg" alt="arrow_bottom_1.svg" height=100 width=100> custom icon "Arrow Botton 1".
-  static SvgPicture arrow_bottom_1 =
-      SvgPicture.asset('assets/icons/arrow_bottom_1.svg');
+  /// <img src="/Users/latrygin/Development/motivation/assets/icons/icons_dark_theme/google-svgrepo-com.svg" alt="arrow_bottom_2.svg" height=100 width=100> custom icon "Google".
+  static SvgPicture google =
+      SvgPicture.asset('assets/icons/icons_dark_theme/google-svgrepo-com.svg');
 
-  /// <img src="/Users/latrygin/Development/motivation/assets/icons/arrow_bottom_2.svg" alt="arrow_bottom_2.svg" height=100 width=100> custom icon "Arrow Botton 2".
-  static SvgPicture arrow_bottom_2 =
-      SvgPicture.asset('assets/icons/arrow_bottom_2.svg');
+  /// <img src="/Users/latrygin/Development/motivation/assets/icons/icons_dark_theme/facebook-svgrepo-com.svg" alt="arrow_bottom_2.svg" height=100 width=100> custom icon "FaceBook".
+  static SvgPicture facebook = SvgPicture.asset(
+      'assets/icons/icons_dark_theme/facebook-svgrepo-com.svg');
+
+  /// <img src="/Users/latrygin/Development/motivation/assets/icons/icons_dark_theme/vk-v2-svgrepo-com.svg" alt="arrow_bottom_2.svg" height=100 width=100> custom icon "VK".
+  static SvgPicture vk =
+      SvgPicture.asset('assets/icons/icons_dark_theme/vk-v2-svgrepo-com.svg');
+
+  /// <img src="/Users/latrygin/Development/motivation/assets/icons/icons_dark_theme/Sun.svg" alt="Sun.svg" height=100 width=100> custom icon "Sun.svg".
+  static SvgPicture sun =
+      SvgPicture.asset('assets/icons/icons_dark_theme/Sun.svg');
+
+  /// <img src="/Users/latrygin/Development/motivation/assets/icons/icons_light_theme/Moon.svg" alt="Moon.svg" height=100 width=100> custom icon "Moon".
+  static SvgPicture moon =
+      SvgPicture.asset('assets/icons/icons_light_theme/Moon.svg');
 }
