4eda8f203951225e86800ec59dca0da87568edd2
latrygin <latrygin@icloud.com>
2022-10-06 17:37:59 +0700

    Add ViewModel and changed structure

---
+++ lib/screens/login_page/login_page.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/material.dart';
+import 'package:motivation/assets/icons/svg.dart';
+import 'package:motivation/screens/login_page/login_page_view_model.dart';
+import 'package:motivation/widgets/frame.dart';
+import 'package:provider/provider.dart';
+import '../../assets/icons/svgs.dart';
+import '../../assets/themes/change_theme.dart';
+import 'package:go_router/go_router.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+        create: (context) => LoginPageViewModel(),
+        child: const FrameWidget(child: _LoginBodyWidget()),
+      );
+}
+
+class _LoginBodyWidget extends StatelessWidget {
+  const _LoginBodyWidget({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final model = context.watch<LoginPageViewModel>();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const _ChangingThemeButtonWidget(),
+        Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text('Войти',
+                      style: Theme.of(context).textTheme.headlineLarge)),
+              const SizedBox(
+                height: 24,
+              ),
+              TextFormField(
+                controller: model.email,
+                style: Theme.of(context).textTheme.headlineSmall,
+                onChanged: (value) => model.resetErrorEmail(),
+                decoration: InputDecoration(
+                    hintText: 'Электронная почта', errorText: model.errorEmail),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              TextFormField(
+                  controller: model.password,
+                  obscureText: model.obscure,
+                  style: Theme.of(context).textTheme.headlineSmall,
+                  onChanged: (value) => model.resetErrorPassword(),
+                  decoration: InputDecoration(
+                      hintText: 'Пароль',
+                      errorText: model.errorPassword,
+                      suffixIcon: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: SVG(
+                          model.obscure
+                              ? Theme.of(context).brightness == Brightness.dark
+                                  ? SVGs.eye_light
+                                  : SVGs.eye_dark
+                              : Theme.of(context).brightness == Brightness.dark
+                                  ? SVGs.eye_slash_light
+                                  : SVGs.eye_slash_dark,
+                          onPressed: () => model.changeObscureValue(),
+                          size: 12,
+                        ),
+                      ))),
+              const SizedBox(
+                height: 24,
+              ),
+              ElevatedButton(
+                onPressed: () => model.onTapButtonLogin(),
+                child: Text(
+                  'Войти',
+                  style: Theme.of(context).textTheme.labelMedium,
+                ),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              Row(
+                children: const [
+                  Expanded(child: Divider()),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Text(
+                      'Или зарегистрироваться\nс помощью',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ),
+                  Expanded(child: Divider())
+                ],
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SVG(SVGs.google, size: 40),
+                  const SizedBox(
+                    width: 28,
+                  ),
+                  SVG(SVGs.twitter, size: 40),
+                  const SizedBox(
+                    width: 28,
+                  ),
+                  SVG(SVGs.facebook, size: 40),
+                  const SizedBox(
+                    width: 28,
+                  ),
+                  SVG(SVGs.vk, size: 40),
+                ],
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(bottom: 32),
+          child: InkWell(
+            onTap: () => context.go('/registration'),
+            child: RichText(
+              text: TextSpan(
+                text: 'Нет аккаунта? ',
+                style: DefaultTextStyle.of(context).style,
+                children: const <TextSpan>[
+                  TextSpan(
+                      text: 'Зарегистрируйся',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFFA35BFF))),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _ChangingThemeButtonWidget extends StatelessWidget {
+  const _ChangingThemeButtonWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.topRight,
+      child: Padding(
+          padding: const EdgeInsets.only(top: 8),
+          child: Consumer<ThemeModel>(
+            builder: (context, ThemeModel themeNotifier, child) {
+              return SVG(
+                Theme.of(context).brightness == Brightness.dark
+                    ? SVGs.sun
+                    : SVGs.moon,
+                onPressed: () {
+                  themeNotifier.isDark
+                      ? themeNotifier.isDark = false
+                      : themeNotifier.isDark = true;
+                },
+                size: 36,
+              );
+            },
+          )),
+    );
+  }
+}
---
+++ lib/screens/login_page/login_page_view_model.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class LoginPageViewModel extends ChangeNotifier {
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+
+  bool obscure = true;
+
+  String? errorPassword;
+  String? errorEmail;
+
+  void changeObscureValue() {
+    obscure ? obscure = false : obscure = true;
+    notifyListeners();
+  }
+
+  void onTapButtonLogin() {
+    if (validateEmail() && validatePassword()) {
+      print(123);
+    }
+  }
+
+  bool validatePassword() {
+    if (password.text.isEmpty) {
+      errorPassword = 'Поле пароля не заполнено';
+      notifyListeners();
+      return false;
+    } else if (password.text.length < 8) {
+      errorPassword = 'Пароль слишком короткий';
+      notifyListeners();
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  bool validateEmail() {
+    if (email.text.isEmpty) {
+      errorEmail = 'Поле почты не заполнено';
+      notifyListeners();
+      return false;
+    } else if (email.text.length < 8) {
+      errorEmail = 'Электронная почта не может быть такой короткой';
+      notifyListeners();
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  void resetErrorPassword() {
+    errorPassword = null;
+    notifyListeners();
+  }
+
+  void resetErrorEmail() {
+    errorEmail = null;
+    notifyListeners();
+  }
+}
