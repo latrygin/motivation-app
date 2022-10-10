a06ef96b8fa354395b02850e251cd1fdf12a8e9c
latrygin <latrygin@icloud.com>
2022-10-10 18:49:09 +0700

    Added second themes for header

--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -1,29 +1,55 @@
 import 'package:flutter/material.dart';
 
 class HeaderWidget extends StatelessWidget {
-  final String title;
+  final String? title;
+  final List<Widget>? actions;
   final List<Widget> children;
-  const HeaderWidget({Key? key, required this.title, required this.children})
+  final bool isShadow;
+  const HeaderWidget(
+      {Key? key,
+      this.title,
+      required this.children,
+      required this.isShadow,
+      this.actions})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 20.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            title,
-            style: Theme.of(context)
-                .textTheme
-                .headlineLarge!
-                .copyWith(fontSize: 24),
-          ),
-          Row(
-            children: children,
-          )
-        ],
+    return Container(
+      padding: const EdgeInsets.only(bottom: 2),
+      color: Colors.transparent,
+      child: Container(
+        decoration: BoxDecoration(
+            color: Theme.of(context).scaffoldBackgroundColor,
+            boxShadow: isShadow
+                ? [
+                    BoxShadow(
+                        color: Theme.of(context).hintColor,
+                        offset: const Offset(0, 2),
+                        blurRadius: 1)
+                  ]
+                : null),
+        padding:
+            const EdgeInsets.only(top: 20.0, bottom: 18.0, left: 16, right: 16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            title != null
+                ? Text(
+                    title ?? '',
+                    style: Theme.of(context)
+                        .textTheme
+                        .headlineLarge!
+                        .copyWith(fontSize: 24),
+                  )
+                : Row(
+                    children: actions ?? [],
+                  ),
+            Row(
+              children: children,
+            )
+          ],
+        ),
       ),
     );
   }
