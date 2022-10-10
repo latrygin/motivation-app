0e30fb1e4dc8d8b54a0b348b857f009849e44a16
latrygin <latrygin@icloud.com>
2022-10-10 18:50:23 +0700

    changed structure

--- lib/screens/forum_page/forum_page.dart
+++ lib/screens/forum_page/forum_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:gif_view/gif_view.dart';
 import 'package:motivation/widgets/header.dart';
 import 'package:motivation/widgets/search_widget.dart';
 import 'package:provider/provider.dart';
@@ -26,29 +25,26 @@ class _ForumBodyWidget extends StatelessWidget {
     final model = context.watch<ForumPageViewModel>();
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            children: [
-              const _HeaderWidget(),
-              Expanded(
-                child: SingleChildScrollView(
-                    controller: model.controller,
-                    physics: const BouncingScrollPhysics(),
-                    child: Column(
-                      children: const [
-                        SearchWidget(
-                          title: 'Поиск по форуму',
-                        ),
-                        SizedBox(
-                          height: 16,
-                        ),
-                        _ListForumWidget(),
-                      ],
-                    )),
-              )
-            ],
-          ),
+        child: Column(
+          children: [
+            const _HeaderWidget(),
+            Expanded(
+              child: SingleChildScrollView(
+                  controller: model.controller,
+                  physics: const BouncingScrollPhysics(),
+                  child: Column(
+                    children: const [
+                      SearchWidget(
+                        title: 'Поиск по форуму',
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      _ListForumWidget(),
+                    ],
+                  )),
+            )
+          ],
         ),
       ),
       floatingActionButton: model.isShowFloatingActionButton
@@ -117,15 +113,12 @@ class _ForumItemWidget extends StatelessWidget {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    AnimatedSwitcher(
-                      duration: Duration(seconds: 5),
-                      child: TextButton(
-                          onPressed: () {},
-                          child: SVG(
-                              Theme.of(context).brightness == Brightness.dark
-                                  ? SVGs.close_light
-                                  : SVGs.close_dark)),
-                    ),
+                    TextButton(
+                        onPressed: () {},
+                        child: SVG(
+                            Theme.of(context).brightness == Brightness.dark
+                                ? SVGs.close_light
+                                : SVGs.close_dark)),
                     TextButton(
                         onPressed: () {},
                         child: SVG(
@@ -150,119 +143,123 @@ class _ForumItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final model = context.read<ForumPageViewModel>();
-    return Container(
-      width: double.infinity,
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-          color: Theme.of(context).hintColor,
-          borderRadius: BorderRadius.circular(12)),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                decoration: BoxDecoration(
-                    color: Theme.of(context).scaffoldBackgroundColor,
-                    borderRadius: BorderRadius.circular(12)),
-                child: Text(
-                  'Игры',
-                  style: Theme.of(context).textTheme.headlineSmall,
-                ),
-              ),
-              GestureDetector(
-                onTapDown: (details) => getDialogWindow(context, details),
-                child: SizedBox(
-                  height: 24,
-                  width: 24,
-                  child: SVG(
-                    Theme.of(context).brightness == Brightness.dark
-                        ? SVGs.unactive_more_light
-                        : SVGs.unactive_more_dark,
-                    //onPressed: () => getDialogWindow(context),
+    //final model = context.read<ForumPageViewModel>();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+            color: Theme.of(context).hintColor,
+            borderRadius: BorderRadius.circular(12)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).scaffoldBackgroundColor,
+                      borderRadius: BorderRadius.circular(12)),
+                  child: Text(
+                    'Игры',
+                    style: Theme.of(context).textTheme.headlineSmall,
                   ),
                 ),
-              )
-            ],
-          ),
-          const SizedBox(
-            height: 8,
-          ),
-          Text(
-            'Что делать, если друг зовёт в доту?',
-            style: Theme.of(context).textTheme.headlineMedium,
-          ),
-          Text(description,
-              maxLines: 3,
-              overflow: TextOverflow.ellipsis,
-              style: Theme.of(context).textTheme.titleSmall),
-          const SizedBox(
-            height: 8,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  Container(
-                    height: 28,
-                    width: 28,
-                    padding: const EdgeInsets.all(6),
-                    decoration: BoxDecoration(
-                        color: Theme.of(context).scaffoldBackgroundColor,
-                        borderRadius: BorderRadius.circular(30)),
+                GestureDetector(
+                  onTapDown: (details) => getDialogWindow(context, details),
+                  child: SizedBox(
+                    height: 24,
+                    width: 24,
                     child: SVG(
                       Theme.of(context).brightness == Brightness.dark
-                          ? SVGs.unactive_user_light
-                          : SVGs.unactive_user_dark,
+                          ? SVGs.unactive_more_light
+                          : SVGs.unactive_more_dark,
+                      //onPressed: () => getDialogWindow(context),
                     ),
                   ),
-                  const SizedBox(
-                    width: 12,
-                  ),
-                  Text(
-                    'Latrygin Arseniy',
-                    style: Theme.of(context).textTheme.headlineSmall,
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Icon(
-                    CupertinoIcons.eye_fill,
-                    size: 16,
-                    color: Colors.grey.shade600,
-                  ),
-                  const SizedBox(
-                    width: 8,
-                  ),
-                  Text(
-                    '342',
-                    style: Theme.of(context).textTheme.headlineSmall,
-                  ),
-                  const SizedBox(
-                    width: 20,
-                  ),
-                  Icon(
-                    CupertinoIcons.hand_thumbsup_fill,
-                    size: 16,
-                    color: Colors.grey.shade600,
-                  ),
-                  const SizedBox(
-                    width: 8,
-                  ),
-                  Text(
-                    '52',
-                    style: Theme.of(context).textTheme.headlineSmall,
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ],
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Text(
+              'Что делать, если друг зовёт в доту?',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            Text(description,
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
+                style: Theme.of(context).textTheme.titleSmall),
+            const SizedBox(
+              height: 8,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      height: 28,
+                      width: 28,
+                      padding: const EdgeInsets.all(6),
+                      decoration: BoxDecoration(
+                          color: Theme.of(context).scaffoldBackgroundColor,
+                          borderRadius: BorderRadius.circular(30)),
+                      child: SVG(
+                        Theme.of(context).brightness == Brightness.dark
+                            ? SVGs.unactive_user_light
+                            : SVGs.unactive_user_dark,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 12,
+                    ),
+                    Text(
+                      'Latrygin Arseniy',
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      CupertinoIcons.eye_fill,
+                      size: 16,
+                      color: Colors.grey.shade600,
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Text(
+                      '342',
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    Icon(
+                      CupertinoIcons.hand_thumbsup_fill,
+                      size: 16,
+                      color: Colors.grey.shade600,
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Text(
+                      '52',
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -273,8 +270,10 @@ class _HeaderWidget extends StatelessWidget {
   static const double size = 28;
   @override
   Widget build(BuildContext context) {
+    final model = context.watch<ForumPageViewModel>();
     return HeaderWidget(
       title: 'Форум',
+      isShadow: model.isShowFloatingActionButton,
       children: [
         SizedBox(
           width: size,
@@ -283,9 +282,7 @@ class _HeaderWidget extends StatelessWidget {
             Theme.of(context).brightness == Brightness.dark
                 ? SVGs.saved_light
                 : SVGs.saved_dark,
-            onPressed: () {
-              print(123);
-            },
+            onPressed: () {},
           ),
         ),
         const SizedBox(
@@ -298,9 +295,7 @@ class _HeaderWidget extends StatelessWidget {
             Theme.of(context).brightness == Brightness.dark
                 ? SVGs.edit_light
                 : SVGs.edit_dark,
-            onPressed: () {
-              print(123);
-            },
+            onPressed: () {},
           ),
         ),
         const SizedBox(
@@ -313,9 +308,7 @@ class _HeaderWidget extends StatelessWidget {
             Theme.of(context).brightness == Brightness.dark
                 ? SVGs.filter_light
                 : SVGs.filter_dark,
-            onPressed: () {
-              print(123);
-            },
+            onPressed: () {},
           ),
         ),
       ],
