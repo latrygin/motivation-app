066dc723d8bac6333ed0b10d722157419b17d404
latrygin <latrygin@icloud.com>
2022-10-10 18:50:55 +0700

    remove import and changed variable for controller

--- lib/screens/forum_page/forum_page_view_model.dart
+++ lib/screens/forum_page/forum_page_view_model.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:gif_view/gif_view.dart';
 
 class ForumPageViewModel extends ChangeNotifier {
   ScrollController controller = ScrollController();
@@ -8,12 +7,12 @@ class ForumPageViewModel extends ChangeNotifier {
   @override
   void addListener(VoidCallback listener) {
     controller.addListener(() {
-      if (controller.position.pixels >= 600 &&
+      if (controller.position.pixels >= 300 &&
           isShowFloatingActionButton == false) {
         isShowFloatingActionButton = true;
         notifyListeners();
       }
-      if (controller.position.pixels <= 600 &&
+      if (controller.position.pixels <= 300 &&
           isShowFloatingActionButton == true) {
         isShowFloatingActionButton = false;
         notifyListeners();
