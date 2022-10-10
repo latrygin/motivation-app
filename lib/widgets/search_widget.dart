2ebc3720688f741a4d3396cb799bf04850b9607e
latrygin <latrygin@icloud.com>
2022-10-10 18:49:46 +0700

    delete padding

--- lib/widgets/search_widget.dart
+++ lib/widgets/search_widget.dart
@@ -8,29 +8,32 @@ class SearchWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTap,
-      borderRadius: BorderRadius.circular(8),
-      child: Container(
-        width: double.infinity,
-        height: 40,
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        decoration: BoxDecoration(
-            color: Theme.of(context).hintColor,
-            borderRadius: BorderRadius.circular(8)),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const Icon(
-              Icons.search,
-              size: 18,
-            ),
-            Text(title),
-            const SizedBox(
-              width: 18,
-            ),
-          ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: InkWell(
+        onTap: onTap,
+        borderRadius: BorderRadius.circular(8),
+        child: Container(
+          width: double.infinity,
+          height: 40,
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          decoration: BoxDecoration(
+              color: Theme.of(context).hintColor,
+              borderRadius: BorderRadius.circular(8)),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Icon(
+                Icons.search,
+                size: 18,
+              ),
+              Text(title),
+              const SizedBox(
+                width: 18,
+              ),
+            ],
+          ),
         ),
       ),
     );
