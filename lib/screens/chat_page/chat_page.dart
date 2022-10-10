7cb6d9cbde6e061715d4a51f4a60aac921719735
latrygin <latrygin@icloud.com>
2022-10-10 18:51:47 +0700

    Changed Structure and Added animation selected items

--- lib/screens/chat_page/chat_page.dart
+++ lib/screens/chat_page/chat_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:motivation/assets/icons/svg.dart';
 import 'package:motivation/assets/icons/svgs.dart';
@@ -21,14 +22,15 @@ class _ChatBodyWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
+    final model = context.watch<ChatPageViewModel>();
+    return Scaffold(
+      body: SafeArea(
         child: Column(
           children: [
             const _HeaderWidget(),
             Expanded(
               child: SingleChildScrollView(
+                  controller: model.controller,
                   physics: const BouncingScrollPhysics(),
                   child: Column(
                     children: const [
@@ -45,6 +47,17 @@ class _ChatBodyWidget extends StatelessWidget {
           ],
         ),
       ),
+      floatingActionButton: model.isShowFloatingActionButton
+          ? FloatingActionButton(
+              backgroundColor: Theme.of(context).primaryColor,
+              onPressed: () => model.scrollUpOnTapFloatingActionButton(),
+              tooltip: 'Подняться наверх',
+              child: const Icon(
+                CupertinoIcons.chevron_up,
+                size: 18,
+              ),
+            )
+          : const SizedBox(),
     );
   }
 }
@@ -59,82 +72,133 @@ class _ListChatsWidget extends StatelessWidget {
         shrinkWrap: true,
         itemCount: 20,
         separatorBuilder: (context, index) {
-          return const Divider();
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 0),
+            child: Divider(
+              height: 1,
+              color: Theme.of(context).hintColor,
+            ),
+          );
         },
         itemBuilder: (context, index) {
-          return const _ChatItemWidget();
+          return _ChatItemWidget(index: index);
         });
   }
 }
 
 class _ChatItemWidget extends StatelessWidget {
-  const _ChatItemWidget({Key? key}) : super(key: key);
+  final int index;
+  const _ChatItemWidget({Key? key, required this.index}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 72,
-      width: double.infinity,
-      alignment: Alignment.center,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Container(
-            height: 60,
-            width: 60,
-            padding: const EdgeInsets.all(16),
-            decoration: BoxDecoration(
-                color: Theme.of(context).hintColor,
-                borderRadius: BorderRadius.circular(30)),
-            child: SVG(
-              Theme.of(context).brightness == Brightness.dark
-                  ? SVGs.unactive_user_light
-                  : SVGs.unactive_user_dark,
-            ),
-          ),
-          const SizedBox(
-            width: 12,
-          ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Anonim',
-                  style: Theme.of(context)
-                      .textTheme
-                      .headlineMedium!
-                      .copyWith(fontSize: 16),
+    final model = context.watch<ChatPageViewModel>();
+    return Material(
+      color: model.isSelectedItem(index)
+          ? Theme.of(context).hintColor
+          : Theme.of(context).scaffoldBackgroundColor,
+      child: InkWell(
+        onLongPress: () => model.selectChatItem(index),
+        onTap: () => model.isModeSelected ? model.selectChatItem(index) : null,
+        child: Container(
+          height: 80,
+          color: Colors.transparent,
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          width: double.infinity,
+          alignment: Alignment.center,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(
+                height: 60,
+                width: 60,
+                child: Stack(
+                  children: [
+                    Container(
+                      height: 60,
+                      width: 60,
+                      padding: const EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                          color: model.isSelectedItem(index)
+                              ? Theme.of(context).scaffoldBackgroundColor
+                              : Theme.of(context).hintColor,
+                          borderRadius: BorderRadius.circular(30)),
+                      child: SVG(
+                        Theme.of(context).brightness == Brightness.dark
+                            ? SVGs.unactive_user_light
+                            : SVGs.unactive_user_dark,
+                      ),
+                    ),
+                    model.isSelectedItem(index)
+                        ? Align(
+                            alignment: Alignment.bottomRight,
+                            child: Container(
+                              height: 18,
+                              width: 18,
+                              decoration: BoxDecoration(
+                                  color: Theme.of(context).primaryColor,
+                                  borderRadius: BorderRadius.circular(20)),
+                              child: const Icon(Icons.done,
+                                  size: 12, color: Colors.white),
+                            ))
+                        : const SizedBox()
+                  ],
                 ),
-                Text(
-                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-                  maxLines: 2,
-                  //textDirection: TextDirection.ltr,
-                  overflow: TextOverflow.ellipsis,
-                  style: Theme.of(context).textTheme.headlineSmall!.copyWith(
-                      fontWeight: FontWeight.w500, color: Colors.grey.shade500),
+              ),
+              const SizedBox(
+                width: 12,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Anonim',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headlineMedium!
+                          .copyWith(fontSize: 16),
+                    ),
+                    Text(
+                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                      maxLines: 2,
+                      //textDirection: TextDirection.ltr,
+                      overflow: TextOverflow.ellipsis,
+                      style: Theme.of(context)
+                          .textTheme
+                          .headlineSmall!
+                          .copyWith(
+                              fontWeight: FontWeight.w500,
+                              color: Colors.grey.shade500),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          ),
-          const SizedBox(
-            width: 12,
-          ),
-          SizedBox(
-            height: 54,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Text(
-                  '10:22',
-                  style: Theme.of(context).textTheme.headlineSmall,
+              ),
+              const SizedBox(
+                width: 12,
+              ),
+              SizedBox(
+                height: 54,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '10:22',
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    ),
+                    Icon(
+                      Icons.done_all,
+                      size: 18,
+                      color: Theme.of(context).primaryColor,
+                    )
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
@@ -145,22 +209,86 @@ class _HeaderWidget extends StatelessWidget {
   static const double size = 28;
   @override
   Widget build(BuildContext context) {
-    return HeaderWidget(
-      title: 'Сообщения',
-      children: [
-        SizedBox(
-          width: 32,
-          height: 32,
-          child: SVG(
-            Theme.of(context).brightness == Brightness.dark
-                ? SVGs.settings_light
-                : SVGs.settings_dark,
-            onPressed: () {
-              print(123);
-            },
-          ),
-        ),
-      ],
-    );
+    final model = context.watch<ChatPageViewModel>();
+    return model.isModeSelected
+        ? HeaderWidget(
+            isShadow: model.isShowFloatingActionButton,
+            actions: [
+              SizedBox(
+                width: size,
+                height: size,
+                child: SVG(
+                  Theme.of(context).brightness == Brightness.dark
+                      ? SVGs.close_light
+                      : SVGs.close_dark,
+                  onPressed: () => model.deletedListItems(),
+                ),
+              ),
+              const SizedBox(
+                width: 12,
+              ),
+              Text(
+                model.selectedItemsList.length.toString(),
+                style: Theme.of(context)
+                    .textTheme
+                    .headlineLarge!
+                    .copyWith(fontSize: 20),
+              )
+            ],
+            children: [
+              SizedBox(
+                width: size,
+                height: size,
+                child: SVG(
+                  Theme.of(context).brightness == Brightness.dark
+                      ? SVGs.trash_light
+                      : SVGs.trash_dark,
+                  onPressed: () {},
+                ),
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              SizedBox(
+                width: size,
+                height: size,
+                child: SVG(
+                  Theme.of(context).brightness == Brightness.dark
+                      ? SVGs.volume_cross_light
+                      : SVGs.volume_cross_dark,
+                  onPressed: () {},
+                ),
+              ),
+              const SizedBox(
+                width: 14,
+              ),
+              SizedBox(
+                width: size,
+                height: size,
+                child: SVG(
+                  Theme.of(context).brightness == Brightness.dark
+                      ? SVGs.volume_high_light
+                      : SVGs.volume_high_dark,
+                  onPressed: () {},
+                ),
+              ),
+            ],
+          )
+        : HeaderWidget(
+            isShadow: model.isShowFloatingActionButton,
+            title: 'Сообщения',
+            children: [
+              SizedBox(
+                width: size,
+                height: size,
+                child: SVG(
+                  Theme.of(context).brightness == Brightness.dark
+                      ? SVGs.settings_light
+                      : SVGs.settings_dark,
+                  onPressed: () {},
+                ),
+              ),
+            ],
+          );
   }
 }
--- lib/screens/chat_page/chat_page_view_model.dart
+++ lib/screens/chat_page/chat_page_view_model.dart
@@ -1,3 +1,49 @@
 import 'package:flutter/material.dart';
 
-class ChatPageViewModel extends ChangeNotifier {}
+class ChatPageViewModel extends ChangeNotifier {
+  ScrollController controller = ScrollController();
+  bool isShowFloatingActionButton = false;
+  List<int> selectedItemsList = [];
+  bool isModeSelected = false;
+
+  void selectChatItem(int index) {
+    if (selectedItemsList.contains(index)) {
+      selectedItemsList.remove(index);
+    } else {
+      selectedItemsList.add(index);
+    }
+    selectedItemsList.isEmpty ? isModeSelected = false : isModeSelected = true;
+    notifyListeners();
+  }
+
+  bool isSelectedItem(int index) => selectedItemsList.contains(index);
+
+  void deletedListItems() {
+    isModeSelected = false;
+    selectedItemsList.clear();
+    notifyListeners();
+  }
+
+  @override
+  void addListener(VoidCallback listener) {
+    controller.addListener(() {
+      if (controller.position.pixels >= 300 &&
+          isShowFloatingActionButton == false) {
+        isShowFloatingActionButton = true;
+        notifyListeners();
+      }
+      if (controller.position.pixels <= 300 &&
+          isShowFloatingActionButton == true) {
+        isShowFloatingActionButton = false;
+        notifyListeners();
+      }
+    });
+    super.addListener(listener);
+  }
+
+  void scrollUpOnTapFloatingActionButton() {
+    controller.animateTo(0,
+        duration: const Duration(milliseconds: 500), curve: Curves.easeIn);
+    notifyListeners();
+  }
+}
