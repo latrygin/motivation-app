5f6714ee0af52371eda40277087f439374106b32
latrygin <latrygin@icloud.com>
2022-10-23 23:19:47 +0700

    Created unit test for login, registration and deleted user

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,43 +1,43 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:motivation/screens/auth_pages/login_page/login_page_view_model.dart';
 
-void main() {
+import 'login_test.dart';
+
+void main() async {
+  //await Hive.initFlutter();
+  //WidgetsFlutterBinding.ensureInitialized();
+  //validationTest();
   loginTest();
+  //tokenProviderTest();
 }
 
-void loginTest() {
-  test('Тестирование валидации почты на незаполненное поле', () {
-    LoginPageViewModel loginViewModel = LoginPageViewModel();
+void validationTest() {
+  // test('Тестирование валидации почты на незаполненное поле', (BuildContext context) {
+  //   LoginPageViewModel loginViewModel = LoginPageViewModel();
 
-    loginViewModel.email.text = '';
+  //   loginViewModel.email.text = '';
 
-    bool isValidationEmail = loginViewModel.validateEmail();
-    expect(isValidationEmail, false);
-  });
+  //   bool isValidationEmail = loginViewModel.validateEmail();
+  //   expect(isValidationEmail, false);
+  // });
 
-  test('Тестирование валидации почты на короткое поле', () {
-    LoginPageViewModel loginViewModel = LoginPageViewModel();
+  // test('Тестирование валидации почты на короткое поле', () {
+  //   LoginPageViewModel loginViewModel = LoginPageViewModel();
 
-    loginViewModel.email.text = '123';
+  //   loginViewModel.email.text = '123';
 
-    bool isValidationEmail = loginViewModel.validateEmail();
-    expect(isValidationEmail, false);
-  });
+  //   bool isValidationEmail = loginViewModel.validateEmail();
+  //   expect(isValidationEmail, false);
+  // });
 
-  test('Тестирование валидации почты на правильно заполненное поле', () {
-    LoginPageViewModel loginViewModel = LoginPageViewModel();
+  // test('Тестирование валидации почты на правильно заполненное поле', () {
+  //   LoginPageViewModel loginViewModel = LoginPageViewModel();
 
-    loginViewModel.email.text = 'latrygin@gmail.com';
+  //   loginViewModel.email.text = 'latrygin@gmail.com';
 
-    bool isValidationEmail = loginViewModel.validateEmail();
-    expect(isValidationEmail, true);
-  });
+  //   bool isValidationEmail = loginViewModel.validateEmail();
+  //   expect(isValidationEmail, true);
+  // });
 }
