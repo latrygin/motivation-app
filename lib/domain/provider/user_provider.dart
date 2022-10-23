f7c6242860657af564501c626ad7de55a17d2e06
latrygin <latrygin@icloud.com>
2022-10-23 23:23:08 +0700

    class user provider
    Created class for saving user data in hive local storage and getting user data

--- lib/domain/provider/user_provider.dart
+++ lib/domain/provider/user_provider.dart
@@ -1,3 +1,67 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:motivation/domain/entity/user.dart';
+
 class UserProvider {
-  Future<void> setUserData() async {}
+  ///Ключ открытия бокса и нахождения
+  ///в под этим ключём в боксе
+  static const String _USER_BOX = 'user_box_key';
+
+  ///Установить данные пользователя в Базу данных
+  ///Hive. Устанавливать при входе и автовходе и вывод
+  ///информации в настройках
+  ///@param [user]
+  ///@return void
+
+  Future<void> setUserDataInLocalStorage(User user) async {
+    //Достать коробку UserAdapter
+
+    var box = await _getAndOpenBoxUserAdapter();
+
+    //Установить параметры пользователя
+
+    box.put(_USER_BOX, user);
+
+    //Закрываем коробку
+
+    box.close();
+  }
+
+  ///Получить данные о пальзователе из базы данных
+  ///Hive. Устанавливать при входе и автовходе и вывод
+  ///информации в настройках
+  //
+  ///@return void
+
+  Future<User> getUserDataFromLocalStorage() async {
+    //Достать коробку UserAdapter
+
+    var box = await _getAndOpenBoxUserAdapter();
+
+    //Достать пользователя по ключу
+
+    var user = box.get(_USER_BOX);
+
+    //Закрываем коробку
+
+    box.close();
+    return user!;
+  }
+
+  ///Проверка регистрации коробки
+  ///и открытие её
+  ///@return [Box<User>]
+  Future<Box<User>> _getAndOpenBoxUserAdapter() async {
+    //Регистрация адаптера
+
+    if (!Hive.isAdapterRegistered(1)) {
+      Hive.registerAdapter(UserAdapter());
+    }
+
+    //Открытие коробки
+
+    var box = await Hive.openBox<User>(_USER_BOX);
+    return box;
+  }
 }
