bd81ff20ffcc2e98996d2e3c7978460defca5703
latrygin <latrygin@icloud.com>
2022-10-27 16:16:25 +0700

    Create forum_services.dart

---
+++ lib/domain/services/forum_services.dart
@@ -0,0 +1,50 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:logger/logger.dart';
+import 'package:motivation/assets/api/url.dart';
+import 'package:motivation/domain/entity/post.dart';
+import 'package:motivation/domain/provider/token_provider.dart';
+
+class ForumServices {
+  final _tokenProvider = TokenProvider();
+
+  ///Объект класса Logger для вызова
+  ///воводов в консоль отладки
+  final _loggerNoStack = Logger(
+    printer: PrettyPrinter(methodCount: 0),
+  );
+
+  ///Получение списка форума
+  ///без фильрации
+  Future<List<Post>> getAllForumListFromApi() async {
+    var header = await _tokenProvider.getHeaderWithToken();
+    var response = await http.get(Url.forum, headers: header);
+    if (response.statusCode == 200) {
+      var items = json.decode(response.body);
+      List<Post> listForumPosts = items.map<Post>((json) {
+        return Post.fromJson(json);
+      }).toList();
+      //_loggerNoStack.v(items);
+      return listForumPosts;
+    } else {
+      throw 'Лист не получен';
+    }
+  }
+
+  ///Получение списка форума
+  ///без фильрации
+  Future<List<Post>> getMyForumListFromApi() async {
+    var header = await _tokenProvider.getHeaderWithToken();
+    var response = await http.get(Url.myforum, headers: header);
+    if (response.statusCode == 200) {
+      var items = json.decode(response.body);
+      List<Post> listForumPosts = items.map<Post>((json) {
+        return Post.fromJson(json);
+      }).toList();
+      //_loggerNoStack.v(items);
+      return listForumPosts;
+    } else {
+      throw 'Лист не получен';
+    }
+  }
+}
