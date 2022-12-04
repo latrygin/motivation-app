869af5f4988fe6090bbba29d9117c6439ac70b94
latrygin <latrygin@icloud.com>
2022-12-04 16:25:43 +0100

    Update bottom_navigation_bar.dart

--- lib/widgets/bottom_navigation_bar.dart
+++ lib/widgets/bottom_navigation_bar.dart
@@ -67,75 +67,55 @@ class BottomNavigationBarWidgetState extends State<BottomNavigationBarWidget> {
           items: <BottomNavigationBarItem>[
             BottomNavigationBarItem(
               icon: SVG(
-                Theme.of(context).brightness == Brightness.dark
-                    ? SVGs.unactive_statistic_light
-                    : SVGs.unactive_statistic_dark,
+                SVGs(context).unactive_statistic,
                 size: sizeicons,
               ),
               activeIcon: SVG(
-                Theme.of(context).brightness == Brightness.dark
-                    ? SVGs.statistic_light
-                    : SVGs.statistic_dark,
+                SVGs(context).statistic,
                 size: sizeicons,
               ),
               label: 'Statistic',
             ),
             BottomNavigationBarItem(
               icon: SVG(
-                Theme.of(context).brightness == Brightness.dark
-                    ? SVGs.unactive_message_light
-                    : SVGs.unactive_message_dark,
+                SVGs(context).unactive_message,
                 size: sizeicons,
               ),
               activeIcon: SVG(
-                Theme.of(context).brightness == Brightness.dark
-                    ? SVGs.message_light
-                    : SVGs.message_dark,
+                SVGs(context).message,
                 size: sizeicons,
               ),
               label: 'Message',
             ),
             BottomNavigationBarItem(
               icon: SVG(
-                Theme.of(context).brightness == Brightness.dark
-                    ? SVGs.unactive_edit_light
-                    : SVGs.unactive_edit_dark,
+                SVGs(context).unactive_edit,
                 size: sizeicons,
               ),
               activeIcon: SVG(
-                Theme.of(context).brightness == Brightness.dark
-                    ? SVGs.edit_light
-                    : SVGs.edit_dark,
+                SVGs(context).edit,
                 size: sizeicons,
               ),
               label: 'Forum',
             ),
             BottomNavigationBarItem(
               icon: SVG(
-                Theme.of(context).brightness == Brightness.dark
-                    ? SVGs.unactive_document_light
-                    : SVGs.unactive_document_dark,
+                SVGs(context).unactive_document,
                 size: sizeicons,
               ),
               activeIcon: SVG(
-                Theme.of(context).brightness == Brightness.dark
-                    ? SVGs.document_light
-                    : SVGs.document_dark,
+                SVGs(context).document,
                 size: sizeicons,
               ),
               label: 'News',
             ),
             BottomNavigationBarItem(
               icon: SVG(
-                Theme.of(context).brightness == Brightness.dark
-                    ? SVGs.unactive_user_light
-                    : SVGs.unactive_user_dark,
+                SVGs(context).unactive_user,
                 size: sizeicons,
               ),
               activeIcon: SVG(
-                Theme.of(context).brightness == Brightness.dark
-                    ? SVGs.user_light
-                    : SVGs.user_dark,
+                SVGs(context).user,
                 size: sizeicons,
               ),
               label: 'Profile',
