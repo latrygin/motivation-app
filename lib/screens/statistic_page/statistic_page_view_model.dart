16c29efd237b24d45332ef7ed6714c761672278e
latrygin <latrygin@icloud.com>
2022-12-15 16:06:30 +0100

    deleted old files statistic page

--- lib/screens/statistic_page/statistic_page_view_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:motivation/domain/entity/user.dart';
-import 'package:motivation/domain/provider/user_provider.dart';
-
-class StatisticPageViewModel extends ChangeNotifier {
-  final _userProvider = UserProvider();
-
-  User get userInformation => _userProvider.getUserFromOpenBox();
-}
