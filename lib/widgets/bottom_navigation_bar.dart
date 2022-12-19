758cf430443a2e2be1c9dba09dba37c55bb050dd
latrygin <latrygin@icloud.com>
2022-12-19 18:36:49 +0100

    Update bottom_navigation_bar.dart

--- lib/widgets/bottom_navigation_bar.dart
+++ lib/widgets/bottom_navigation_bar.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:motivation/assets/icons/svg.dart';
 import 'package:motivation/assets/icons/svgs.dart';
-import 'package:motivation/screens/chat_page/chat_group/chat_page.dart';
-import 'package:motivation/screens/forum_page/forum_page.dart';
+import 'package:motivation/screens/chat/view/chat_page.dart';
 import 'package:motivation/screens/profile_page/profile_page.dart';
-import 'package:motivation/screens/statistic_page/statistic_page.dart';
+import 'package:motivation/screens/statistic/statistic/statistic.dart';
 import '../screens/blog_page/blog_page.dart';
-import 'frame.dart';
+import 'wrapper.dart';
 
 class BottomNavigationBarWidget extends StatefulWidget {
   const BottomNavigationBarWidget({super.key});
@@ -35,14 +34,16 @@ class BottomNavigationBarWidgetState extends State<BottomNavigationBarWidget> {
   static const double sizeicons = 28;
 
   static const List<Widget> _widgetOptions = <Widget>[
-    StatisticScreen(),
-    ChatScreen(),
-    ForumScreen(),
+    StatisticPage(),
+    ChatPage(),
+    Text('data'),
+    //ForumScreen(),
     BlogScreen(),
     ProfileScreen(),
   ];
 
   void _onItemTapped(int index) {
+    debugPrint(index.toString());
     setState(() {
       _selectedIndex = index;
       _pageController.jumpToPage(index);
@@ -51,90 +52,89 @@ class BottomNavigationBarWidgetState extends State<BottomNavigationBarWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return FrameForBottomNavigationItemWidget(
-      child: Scaffold(
-        body: SizedBox(
-          child: PageView(
-              controller: _pageController,
-              onPageChanged: (index) {
-                setState(() => _selectedIndex = index);
-              },
-              children: _widgetOptions),
+    return Wrapper(
+      backgroundColor: _selectedIndex == 0 || _selectedIndex == 4
+          ? Theme.of(context).hintColor
+          : Colors.transparent,
+      bottom: false,
+      child: SizedBox(
+        child: PageView(
+          controller: _pageController,
+          onPageChanged: (index) {
+            setState(() => _selectedIndex = index);
+          },
+          children: _widgetOptions,
         ),
-        bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: Theme.of(context).hintColor,
-          type: BottomNavigationBarType.fixed,
-          items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: SVG(
-                SVGs(context).unactive_statistic,
-                size: sizeicons,
-              ),
-              activeIcon: SVG(
-                SVGs(context).statistic,
-                size: sizeicons,
-              ),
-              label: 'Statistic',
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).hintColor,
+        type: BottomNavigationBarType.fixed,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: SVG(
+              SVGs(context).unactive_statistic,
+              size: sizeicons,
             ),
-            BottomNavigationBarItem(
-              icon: SVG(
-                SVGs(context).unactive_message,
-                size: sizeicons,
-              ),
-              activeIcon: SVG(
-                SVGs(context).message,
-                size: sizeicons,
-              ),
-              label: 'Message',
+            activeIcon: SVG(
+              SVGs(context).statistic,
+              size: sizeicons,
             ),
-            BottomNavigationBarItem(
-              icon: SVG(
-                SVGs(context).unactive_edit,
-                size: sizeicons,
-              ),
-              activeIcon: SVG(
-                SVGs(context).edit,
-                size: sizeicons,
-              ),
-              label: 'Forum',
+            label: 'Statistic',
+          ),
+          BottomNavigationBarItem(
+            icon: SVG(
+              SVGs(context).unactive_message,
+              size: sizeicons,
             ),
-            BottomNavigationBarItem(
-              icon: SVG(
-                SVGs(context).unactive_document,
-                size: sizeicons,
-              ),
-              activeIcon: SVG(
-                SVGs(context).document,
-                size: sizeicons,
-              ),
-              label: 'News',
+            activeIcon: SVG(
+              SVGs(context).message,
+              size: sizeicons,
             ),
-            BottomNavigationBarItem(
-              icon: SVG(
-                SVGs(context).unactive_user,
-                size: sizeicons,
-              ),
-              activeIcon: SVG(
-                SVGs(context).user,
-                size: sizeicons,
-              ),
-              label: 'Profile',
+            label: 'Message',
+          ),
+          BottomNavigationBarItem(
+            icon: SVG(
+              SVGs(context).unactive_edit,
+              size: sizeicons,
             ),
-          ],
-          selectedItemColor: Theme.of(context).primaryColor,
-          selectedLabelStyle: Theme.of(context)
-              .textTheme
-              .headlineMedium!
-              .copyWith(fontSize: 12),
-          unselectedLabelStyle: Theme.of(context)
-              .textTheme
-              .headlineMedium!
-              .copyWith(fontSize: 12),
-          showSelectedLabels: true,
-          showUnselectedLabels: true,
-          currentIndex: _selectedIndex,
-          onTap: _onItemTapped,
-        ),
+            activeIcon: SVG(
+              SVGs(context).edit,
+              size: sizeicons,
+            ),
+            label: 'Forum',
+          ),
+          BottomNavigationBarItem(
+            icon: SVG(
+              SVGs(context).unactive_document,
+              size: sizeicons,
+            ),
+            activeIcon: SVG(
+              SVGs(context).document,
+              size: sizeicons,
+            ),
+            label: 'News',
+          ),
+          BottomNavigationBarItem(
+            icon: SVG(
+              SVGs(context).unactive_user,
+              size: sizeicons,
+            ),
+            activeIcon: SVG(
+              SVGs(context).user,
+              size: sizeicons,
+            ),
+            label: 'Profile',
+          ),
+        ],
+        selectedItemColor: Theme.of(context).primaryColor,
+        selectedLabelStyle:
+            Theme.of(context).textTheme.headlineMedium!.copyWith(fontSize: 12),
+        unselectedLabelStyle:
+            Theme.of(context).textTheme.headlineMedium!.copyWith(fontSize: 12),
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
   }
