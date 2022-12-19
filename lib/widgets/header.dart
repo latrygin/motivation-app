228851b7425975b3c3bf18d76fe49dedd70d16d8
latrygin <latrygin@icloud.com>
2022-12-19 18:36:42 +0100

    Update header.dart

--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -5,13 +5,13 @@ class HeaderWidget extends StatelessWidget {
   final List<Widget>? actions;
   final List<Widget> children;
   final bool isShadow;
-  const HeaderWidget(
-      {Key? key,
-      this.title,
-      required this.children,
-      required this.isShadow,
-      this.actions})
-      : super(key: key);
+  const HeaderWidget({
+    Key? key,
+    this.title,
+    required this.children,
+    required this.isShadow,
+    this.actions,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -21,31 +21,34 @@ class HeaderWidget extends StatelessWidget {
       height: 68,
       child: Container(
         decoration: BoxDecoration(
-            color: Theme.of(context).scaffoldBackgroundColor,
-            boxShadow: isShadow
-                ? [
-                    BoxShadow(
-                        color: Theme.of(context).hintColor,
-                        offset: const Offset(0, 2),
-                        blurRadius: 1)
-                  ]
-                : null),
+          color: Theme.of(context).scaffoldBackgroundColor,
+          boxShadow: isShadow
+              ? [
+                  BoxShadow(
+                    color: Theme.of(context).hintColor,
+                    offset: const Offset(0, 2),
+                    blurRadius: 1,
+                  )
+                ]
+              : null,
+        ),
         padding:
             const EdgeInsets.only(top: 20.0, bottom: 18.0, left: 16, right: 16),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            title != null
-                ? Text(
-                    title ?? '',
-                    style: Theme.of(context)
-                        .textTheme
-                        .headlineLarge!
-                        .copyWith(fontSize: 24),
-                  )
-                : Row(
-                    children: actions ?? [],
-                  ),
+            if (title != null)
+              Text(
+                title ?? '',
+                style: Theme.of(context)
+                    .textTheme
+                    .headlineLarge!
+                    .copyWith(fontSize: 24),
+              )
+            else
+              Row(
+                children: actions ?? [],
+              ),
             Row(
               children: children,
             )
