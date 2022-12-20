ead294d1782c80dff66276cc9b2cbceaec610739
latrygin <latrygin@icloud.com>
2022-12-20 09:07:33 +0100

    Removed functionality (rewrite it to the bloc in future)

--- lib/screens/profile_page/profile_page_view_model.dart
+++ lib/screens/profile_page/profile_page_view_model.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/cupertino.dart';
-import 'package:go_router/go_router.dart';
 import 'package:motivation/domain/provider/token_provider.dart';
-import 'package:motivation/domain/provider/user_provider.dart';
 
 class ProfilePageViewModel extends ChangeNotifier {
-  final _userProvider = UserProvider();
-  final _tokenProvider = TokenProvider();
+  //final _userProvider = UserProvider();
   final BuildContext context;
+  final _tokenProvider = TokenProvider();
   ProfilePageViewModel({required this.context});
 
   Future<void> logOutUserAndExitProfile() async {
-    await _userProvider.logOutUser();
-    await _tokenProvider.deleteTokenFromSecureStore();
-    context.go('/');
+    // await _userProvider.deleteUser();
+    await _tokenProvider.deleteToken();
+    //context.go('/');
   }
 }
