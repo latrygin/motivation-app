21064a1f6273fe0c0335890d8a3e7d0ae105eb4f
latrygin <latrygin@icloud.com>
2022-12-20 08:57:24 +0100

    Update message_services.dart

--- lib/domain/services/message_services.dart
+++ lib/domain/services/message_services.dart
@@ -1,31 +1,22 @@
-// ignore_for_file: depend_on_referenced_packages
+// ignore_for_file: depend_on_referenced_packages, constant_identifier_names
 
 import 'dart:convert';
 
 import 'package:motivation/assets/api/url.dart';
-import 'package:motivation/domain/entity/message.dart';
+import 'package:motivation/domain/entity/chat/message.dart';
 import 'package:http/http.dart' as http;
+import 'package:motivation/domain/middleware/response_middleware.dart';
 import 'package:motivation/domain/provider/token_provider.dart';
 
 class MessageServices implements MessageServicesInterface {
   final _tokenProvider = TokenProvider();
-  static const String MESSAGE_ERROR =
-      'Неизвестная ошибка. Мы решаем эту проблему';
-
+  final _middleware = ResponseMiddleware();
   @override
   Future<dynamic> getMessages(int chatId) async {
-    var token = await _tokenProvider.getHeaderWithToken();
-    var response = await http.get(Url.messages(chatId), headers: token);
-    if (response.statusCode == 200) {
-      var items = json.decode(response.body);
-      List<Message> listMessage = items.map<Message>((json) {
-        return Message.fromMap(json);
-      }).toList();
-
-      return listMessage;
-    } else {
-      return MESSAGE_ERROR;
-    }
+    final token = await _tokenProvider.getHeaderWithToken();
+    final response = await http.get(Url.messages(chatId), headers: token);
+    //Переделать
+    _middleware.checkResponse(response.statusCode);
   }
 
   @override
