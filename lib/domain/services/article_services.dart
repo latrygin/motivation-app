1c876d435238cb02d9bae0d1a90d3216af8979c3
latrygin <latrygin@icloud.com>
2022-12-20 08:57:16 +0100

    Update article_services.dart

--- lib/domain/services/article_services.dart
+++ lib/domain/services/article_services.dart
@@ -2,79 +2,89 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:motivation/assets/api/url.dart';
-import 'package:motivation/domain/entity/post.dart';
 import 'package:motivation/domain/provider/token_provider.dart';
 
-/**
- * 1. Написать запрос на
- *    ПОЛУЧЕНИЕ статьи.
- * 2. Написать запрос на
- *    ИЗМЕНЕНИЕ статьи.
- * 3. Написать запрос на 
- *    УДАЛЕНИЕ статьи.
- * 4. Написать запрос на 
- *    СОЗДАНИЕ статьи.
- */
+import '../entity/article/article.dart';
+import '../entity/article/article_response.dart';
+import '../middleware/response_middleware.dart';
 
 class ArticleServices implements ArticleServicesInterface {
   final _tokenProvider = TokenProvider();
-  static const String MESSAGE_ERROR =
-      'Неизвестная ошибка. Мы решаем эту проблему';
+  final _middleware = ResponseMiddleware();
 
   @override
-  Future<dynamic> getArticles(TypeArticle type, int page) async {
-    var header = await _tokenProvider.getHeaderWithToken();
+  Future<ArticleResponse> getArticles(TypeArticle type, int page) async {
+    final header = await _tokenProvider.getHeaderWithToken();
     http.Response response;
     switch (type) {
-      case TypeArticle.New:
+      case TypeArticle.news:
         response = await http.get(Url.new_article(page), headers: header);
         break;
-      case TypeArticle.Popular:
+      case TypeArticle.popular:
         response = await http.get(Url.popular_article(page), headers: header);
         break;
-      case TypeArticle.My:
+      case TypeArticle.my:
         response = await http.get(Url.my_article(page), headers: header);
         break;
       default:
-        throw 'Неизвестный тип';
+        throw UnimplementedError();
     }
-
-    if (response.statusCode == 200) {
-      var items = json.decode(response.body);
-      List<Post> listForumPosts = items.map<Post>((json) {
-        return Post.fromJson(json);
-      }).toList();
-      return listForumPosts;
-    } else {
-      return MESSAGE_ERROR;
-    }
+    //Переделать
+    _middleware.checkResponse(response.statusCode);
+    final items = json.decode(response.body) as Map<String, dynamic>;
+    return ArticleResponse.fromJson(items);
   }
 
   @override
-  Future<void> createArticle(Map<String, dynamic> body) {
-    throw UnimplementedError();
+  Future<void> createArticle(Map<String, dynamic> body) async {
+    final header = await _tokenProvider.getHeaderWithToken();
+    final response = await http.post(
+      Url.post_article(),
+      headers: header,
+      body: jsonEncode(body),
+    );
+    //Переделать
+    _middleware.checkResponse(response.statusCode);
   }
 
   @override
-  Future<void> deleteArticle(int id) {
-    throw UnimplementedError();
+  Future<void> deleteArticle(int id) async {
+    final header = await _tokenProvider.getHeaderWithToken();
+    final response = await http.delete(
+      Url.delete_article(id),
+      headers: header,
+    );
+    //Переделать
+    _middleware.checkResponse(response.statusCode);
   }
 
   @override
-  Future getArticle(int id) {
-    throw UnimplementedError();
+  Future<Article> getArticle(int id) async {
+    final header = await _tokenProvider.getHeaderWithToken();
+    final response = await http.get(Url.get_article(id), headers: header);
+    //Переделать
+    _middleware.checkResponse(response.statusCode);
+    final items = json.decode(response.body) as Map<String, dynamic>;
+    return Article.fromJson(items);
   }
 
   @override
-  Future<void> putArticle(int id, Map<String, dynamic> body) {
-    throw UnimplementedError();
+  Future<void> putArticle(int id, Map<String, dynamic> body) async {
+    final header = await _tokenProvider.getHeaderWithToken();
+    final response = await http.put(
+      Url.put_article(id),
+      headers: header,
+      body: jsonEncode(body),
+    );
+    //Переделать
+    _middleware.checkResponse(response.statusCode);
   }
 }
 
 abstract class ArticleServicesInterface {
-  Future<dynamic> getArticles(TypeArticle type, int page);
+  Future<ArticleResponse> getArticles(TypeArticle type, int page);
 
-  Future<dynamic> getArticle(int id);
+  Future<Article> getArticle(int id);
 
   Future<void> createArticle(Map<String, dynamic> body);
 
@@ -83,4 +93,4 @@ abstract class ArticleServicesInterface {
   Future<void> deleteArticle(int id);
 }
 
-enum TypeArticle { New, My, Popular }
+enum TypeArticle { news, my, popular }
