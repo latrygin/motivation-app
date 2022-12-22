9cb208d3e32a5825c532f8b5ac595f8fccc169cb
latrygin <latrygin@icloud.com>
2022-12-22 17:40:57 +0100

    Update statistic_page.dart

--- lib/screens/statistic/statistic/view/statistic_page.dart
+++ lib/screens/statistic/statistic/view/statistic_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:motivation/widgets/wrapper.dart';
 
 import '../bloc/statistic_bloc.dart';
 import 'statistic_body.dart';
