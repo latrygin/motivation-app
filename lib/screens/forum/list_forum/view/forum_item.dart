9983a16f0def49da96a12e516aae6aab2ae9994f
latrygin <latrygin@icloud.com>
2022-12-22 17:40:36 +0100

    Update forum_item.dart

--- lib/screens/forum/list_forum/view/forum_item.dart
+++ lib/screens/forum/list_forum/view/forum_item.dart
@@ -3,61 +3,20 @@ import 'package:motivation/assets/icons/svg.dart';
 import 'package:motivation/assets/icons/svgs.dart';
 import 'package:motivation/domain/entity/forum/forum.dart';
 
-class ForumItemWidget extends StatelessWidget {
+enum SampleItem { itemOne, itemTwo, itemThree }
+
+class ForumItemWidget extends StatefulWidget {
   final Forum forum;
-  const ForumItemWidget({Key? key, required this.forum}) : super(key: key);
+  const ForumItemWidget({
+    Key? key,
+    required this.forum,
+  }) : super(key: key);
 
-  void getDialogWindow(BuildContext context, TapDownDetails details) {
-    showDialog<void>(
-      useSafeArea: false,
-      context: context,
-      builder: (context) {
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Padding(
-              padding: EdgeInsets.only(
-                right: 28,
-                top: details.globalPosition.dy - 50,
-              ),
-              child: Container(
-                height: 50,
-                width: 160,
-                alignment: Alignment.center,
-                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                decoration: BoxDecoration(
-                  color: Theme.of(context).hintColor,
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(25),
-                    topRight: Radius.circular(25),
-                    bottomLeft: Radius.circular(25),
-                  ),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    TextButton(
-                      onPressed: () {},
-                      child: SVG(SVGs(context).close),
-                    ),
-                    TextButton(
-                      onPressed: () {},
-                      child: SVG(SVGs(context).saved),
-                    ),
-                    TextButton(
-                      onPressed: () {},
-                      child: SVG(SVGs(context).send),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        );
-      },
-    );
-  }
+  @override
+  State<ForumItemWidget> createState() => _ForumItemWidgetState();
+}
 
+class _ForumItemWidgetState extends State<ForumItemWidget> {
   String differenceDateTime(DateTime time) {
     final now = DateTime.now();
     final dif = now.difference(time);
@@ -72,150 +31,208 @@ class ForumItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: Theme.of(context).scaffoldBackgroundColor,
-        //color: Theme.of(context).hintColor,
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Material(
+      color: Theme.of(context).scaffoldBackgroundColor,
+      elevation: 2,
+      child: InkWell(
+        onTap: () => debugPrint('material'),
+        child: Container(
+          width: double.infinity,
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Container(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 10,
-                      vertical: 2,
-                    ),
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).hintColor,
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Text(
-                      forum.theme,
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 8,
-                  ),
-                  Text(
-                    differenceDateTime(forum.created_at),
-                    style: Theme.of(context).textTheme.titleSmall,
-                  ),
-                ],
-              ),
-              GestureDetector(
-                onTapDown: (details) => getDialogWindow(context, details),
-                child: SVG(
-                  SVGs(context).more,
-                  onPressed: () {},
-                ),
-              )
-            ],
-          ),
-          const SizedBox(
-            height: 8,
-          ),
-          Text(
-            forum.title,
-            maxLines: 2,
-            style: Theme.of(context).textTheme.headlineMedium,
-          ),
-          const SizedBox(
-            height: 8,
-          ),
-          Text(
-            forum.description!,
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
-            style: Theme.of(context).textTheme.titleSmall,
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  SVG(
-                    SVGs(context).unactive_message,
-                    onPressed: () {},
-                    size: 22,
-                  ),
-                  const SizedBox(
-                    width: 8,
-                  ),
-                  Text(
-                    forum.count_message.toString(),
-                    style: Theme.of(context)
-                        .textTheme
-                        .headlineLarge!
-                        .copyWith(fontSize: 14),
-                  ),
-                  const SizedBox(
-                    width: 16,
-                  ),
-                  SVG(
-                    SVGs(context).unactive_eye,
-                    onPressed: () {},
-                    size: 22,
-                  ),
-                  const SizedBox(
-                    width: 8,
-                  ),
-                  Text(
-                    forum.count_view.toString(),
-                    style: Theme.of(context)
-                        .textTheme
-                        .headlineLarge!
-                        .copyWith(fontSize: 14),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  SVG(
-                    SVGs(context).chevron_down,
-                    size: 32,
-                  ),
-                  // const Icon(
-                  //   CupertinoIcons.chevron_down,
-                  //   size: 20,
-                  // ),
-                  const SizedBox(
-                    width: 12,
-                  ),
-                  Text(
-                    (forum.count_up - forum.count_down).toString(),
-                    style: Theme.of(context).textTheme.headlineLarge!.copyWith(
-                          fontSize: 14,
-                          color: forum.count_up - forum.count_down >= 0
-                              ? Colors.green
-                              : Colors.red,
+                  Row(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 10,
+                          vertical: 2,
                         ),
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).hintColor,
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Text(
+                          widget.forum.theme,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Text(
+                        differenceDateTime(widget.forum.created_at),
+                        style: Theme.of(context).textTheme.titleSmall,
+                      ),
+                    ],
                   ),
-                  const SizedBox(
-                    width: 12,
+                  PopupMenuButton<SampleItem>(
+                    icon: SVG(
+                      SVGs(context).more,
+                      size: 24,
+                    ),
+                    tooltip: 'Меню',
+                    splashRadius: 24,
+                    elevation: 3,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+                    onSelected: (item) {
+                      debugPrint(item.toString());
+                    },
+                    itemBuilder: (context) => [
+                      const PopupMenuItem<SampleItem>(
+                        value: SampleItem.itemOne,
+                        child: Text('Добавить в избранное'),
+                      ),
+                      const PopupMenuItem<SampleItem>(
+                        value: SampleItem.itemTwo,
+                        child: Text('Читать позже'),
+                      ),
+                      const PopupMenuItem<SampleItem>(
+                        value: SampleItem.itemThree,
+                        child: Text('Отправить'),
+                      ),
+                      const PopupMenuItem<SampleItem>(
+                        value: SampleItem.itemThree,
+                        child: Text('Пожаловаться'),
+                      ),
+                    ],
                   ),
-                  SVG(
-                    SVGs(context).chevron_up,
-                    size: 32,
-                  ),
-                  // const Icon(
-                  //   CupertinoIcons.chevron_up,
-                  //   size: 20,
-                  // ),
                 ],
-              )
+              ),
+              Text(
+                widget.forum.title,
+                maxLines: 2,
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Text(
+                widget.forum.description!,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: Theme.of(context).textTheme.titleSmall,
+              ),
+              if (widget.forum.image == null)
+                Padding(
+                  padding: const EdgeInsets.only(top: 12.0, bottom: 4.0),
+                  child: Container(
+                    width: double.infinity,
+                    height: 240,
+                    color: Theme.of(context).hintColor,
+                    child: Icon(
+                      Icons.photo,
+                      size: 58,
+                      color: Theme.of(context).scaffoldBackgroundColor,
+                    ),
+                  ),
+                )
+              else
+                Padding(
+                  padding: const EdgeInsets.only(top: 12.0, bottom: 4.0),
+                  child: Image.network(
+                    widget.forum.image!,
+                    loadingBuilder: (context, child, loadingProgress) {
+                      if (loadingProgress == null) return child;
+                      return Container(
+                        width: double.infinity,
+                        height: 240,
+                        color: Theme.of(context).hintColor,
+                        child: Icon(
+                          Icons.photo,
+                          color: Theme.of(context).scaffoldBackgroundColor,
+                        ),
+                      );
+                    },
+                    errorBuilder: (context, error, stackTrace) {
+                      return const SizedBox();
+                    },
+                    width: double.infinity,
+                    height: 240,
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      SVG(
+                        SVGs(context).unactive_message,
+                        onPressed: () {},
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Text(
+                        widget.forum.count_message.toString(),
+                        style: Theme.of(context)
+                            .textTheme
+                            .headlineLarge!
+                            .copyWith(fontSize: 14),
+                      ),
+                      const SizedBox(
+                        width: 16,
+                      ),
+                      SVG(
+                        SVGs(context).unactive_eye,
+                        onPressed: () {},
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Text(
+                        widget.forum.count_view.toString(),
+                        style: Theme.of(context)
+                            .textTheme
+                            .headlineLarge!
+                            .copyWith(fontSize: 14),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      SVG(
+                        SVGs(context).chevron_down,
+                        size: 40,
+                        onPressed: () => debugPrint('Yf123'),
+                      ),
+                      const SizedBox(
+                        width: 16,
+                      ),
+                      Text(
+                        (widget.forum.count_up - widget.forum.count_down)
+                            .toString(),
+                        style:
+                            Theme.of(context).textTheme.headlineLarge!.copyWith(
+                                  fontSize: 14,
+                                  color: widget.forum.count_up -
+                                              widget.forum.count_down >=
+                                          0
+                                      ? Colors.green
+                                      : Colors.red,
+                                ),
+                      ),
+                      const SizedBox(
+                        width: 16,
+                      ),
+                      SVG(
+                        SVGs(context).chevron_up,
+                        size: 40,
+                        onPressed: () => debugPrint('Yf123'),
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ],
           ),
-        ],
+        ),
       ),
     );
   }
