26717a9c04f1c8336109f0b7eae6c3cbf949c192
latrygin <latrygin@icloud.com>
2022-12-23 19:10:26 +0100

    Create create_forum_page.dart

---
+++ lib/screens/forum/create_forum/view/create_forum_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:motivation/screens/forum/create_forum/bloc/create_forum_bloc.dart';
+import 'package:motivation/widgets/wrapper.dart';
+
+import 'create_forum_body.dart';
+
+class CreateForumPage extends StatelessWidget {
+  const CreateForumPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrapper(
+      child: BlocProvider(
+        create: (context) => CreateForumBloc(),
+        child: const CreateForumBody(),
+      ),
+    );
+  }
+}
