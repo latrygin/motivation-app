1f84fe9caecbf507c9a09c67f587529a16e9356a
latrygin <latrygin@icloud.com>
2022-12-24 14:38:51 +0100

    Update second_create.dart

--- lib/screens/forum/create_forum/view/create_steps/second_create.dart
+++ lib/screens/forum/create_forum/view/create_steps/second_create.dart
@@ -2,6 +2,9 @@
 
 import 'package:chip_list/chip_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../bloc/create_forum_bloc.dart';
 
 class SecondForumPage extends StatelessWidget {
   const SecondForumPage({super.key});
@@ -33,54 +36,64 @@ class SecondForumPage extends StatelessWidget {
   }
 }
 
-class _ChipListWidget extends StatelessWidget {
+class _ChipListWidget extends StatefulWidget {
   const _ChipListWidget({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      child: ChipList(
-        listOfChipNames: const [
-          'item 1',
-          'item 2',
-          'item 3',
-          'item 4',
-          'item 5',
-          'item 6',
-          'item 7',
-          'item 8',
-          'item 9',
-          'item 10',
-          'item 11',
-          'item 12',
-          'item 13',
-          'item 14',
-          'item 15',
+  State<_ChipListWidget> createState() => _ChipListWidgetState();
+}
 
-          // 'Друзья',
-          // 'Отношения',
-          // 'Семья',
-          // 'Девушка',
-          // 'Парень',
-          // 'Зависимость',
-          // 'Курение',
-          // 'История',
-          // 'Наркотики',
-          // 'Общение',
-          // 'Паника',
-          // 'Другое',
-        ],
-        activeBgColorList: [Theme.of(context).primaryColor],
-        inactiveBgColorList: [Theme.of(context).hintColor.withOpacity(0.5)],
-        inactiveBorderColorList: [Theme.of(context).hintColor],
-        borderRadiiList: [50],
-        inactiveTextColorList: [Theme.of(context).hoverColor],
-        listOfChipIndicesCurrentlySeclected: [0],
-        shouldWrap: true,
-        runSpacing: 10,
-        spacing: 10,
-      ),
+class _ChipListWidgetState extends State<_ChipListWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<CreateForumBloc, CreateForumState>(
+      builder: (context, state) {
+        return SizedBox(
+          width: double.infinity,
+          child: ChipList(
+            extraOnToggle: (theme) =>
+                context.read<CreateForumBloc>().add(ForumThemeChanged(theme)),
+            listOfChipNames: const [
+              'item 1',
+              'item 2',
+              'item 3',
+              'item 4',
+              'item 5',
+              'item 6',
+              'item 7',
+              'item 8',
+              'item 9',
+              'item 10',
+              'item 11',
+              'item 12',
+              'item 13',
+              'item 14',
+              'item 15',
+              // 'Друзья',
+              // 'Отношения',
+              // 'Семья',
+              // 'Девушка',
+              // 'Парень',
+              // 'Зависимость',
+              // 'Курение',
+              // 'История',
+              // 'Наркотики',
+              // 'Общение',
+              // 'Паника',
+              // 'Другое',
+            ],
+            activeBgColorList: [Theme.of(context).primaryColor],
+            inactiveBgColorList: [Theme.of(context).hintColor.withOpacity(0.5)],
+            inactiveBorderColorList: [Theme.of(context).hintColor],
+            borderRadiiList: [50],
+            inactiveTextColorList: [Theme.of(context).hoverColor],
+            listOfChipIndicesCurrentlySeclected: [state.theme],
+            shouldWrap: true,
+            runSpacing: 10,
+            spacing: 10,
+          ),
+        );
+      },
     );
   }
 }
