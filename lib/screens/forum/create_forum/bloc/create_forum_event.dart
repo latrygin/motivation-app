8a5e4f0c4629bc534396e7c824e2c2128388a503
latrygin <latrygin@icloud.com>
2022-12-24 14:38:34 +0100

    Update create_forum_event.dart

--- lib/screens/forum/create_forum/bloc/create_forum_event.dart
+++ lib/screens/forum/create_forum/bloc/create_forum_event.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: non_constant_identifier_names
+
 part of 'create_forum_bloc.dart';
 
 abstract class CreateForumEvent extends Equatable {
@@ -7,10 +9,61 @@ abstract class CreateForumEvent extends Equatable {
   List<Object> get props => [];
 }
 
-class ScrollPageEvent extends CreateForumEvent {
-  final int page;
-  const ScrollPageEvent(this.page);
+class ForumTitleChanged extends CreateForumEvent {
+  const ForumTitleChanged(this.title);
+
+  final String title;
 
   @override
-  List<Object> get props => [page];
+  List<Object> get props => [title];
+}
+
+class ForumIsAnonymousChanged extends CreateForumEvent {
+  const ForumIsAnonymousChanged(this.is_anonymous);
+
+  final bool is_anonymous;
+
+  @override
+  List<Object> get props => [is_anonymous];
+}
+
+class ForumIsActiveChanged extends CreateForumEvent {
+  const ForumIsActiveChanged(this.is_active);
+
+  final bool is_active;
+
+  @override
+  List<Object> get props => [is_active];
+}
+
+class ForumThemeChanged extends CreateForumEvent {
+  const ForumThemeChanged(this.theme);
+
+  final int theme;
+
+  @override
+  List<Object> get props => [theme];
+}
+
+class ForumBodyChanged extends CreateForumEvent {
+  const ForumBodyChanged(this.body);
+
+  final String body;
+
+  @override
+  List<Object> get props => [body];
+}
+
+class ForumOpenGallary extends CreateForumEvent {
+  const ForumOpenGallary();
+
+  @override
+  List<Object> get props => [];
+}
+
+class ForumInitialGallary extends CreateForumEvent {
+  const ForumInitialGallary();
+
+  @override
+  List<Object> get props => [];
 }
