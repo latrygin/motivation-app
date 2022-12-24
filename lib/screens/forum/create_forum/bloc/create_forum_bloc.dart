0688143e16af866a98620e8b6172189e7a22eb12
latrygin <latrygin@icloud.com>
2022-12-24 14:38:32 +0100

    Update create_forum_bloc.dart

--- lib/screens/forum/create_forum/bloc/create_forum_bloc.dart
+++ lib/screens/forum/create_forum/bloc/create_forum_bloc.dart
@@ -1,23 +1,78 @@
 import 'dart:async';
 
 import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
+import 'package:photo_manager/photo_manager.dart';
 part 'create_forum_event.dart';
 part 'create_forum_state.dart';
 
 class CreateForumBloc extends Bloc<CreateForumEvent, CreateForumState> {
   CreateForumBloc() : super(const CreateForumState()) {
     on<CreateForumEvent>((event, emit) {});
-    on<ScrollPageEvent>(_onScrollPage);
+    on<ForumTitleChanged>(_forumTitleChanged);
+    on<ForumIsAnonymousChanged>(_forumIsAnonymousChanged);
+    on<ForumIsActiveChanged>(_forumIsActiveChanged);
+    on<ForumThemeChanged>(_forumThemeChanged);
+    on<ForumBodyChanged>(_forumBodyChanged);
+    on<ForumOpenGallary>(_forumOpenGallary);
   }
 
-  void _onScrollPage(
-    ScrollPageEvent event,
+  void _forumTitleChanged(
+    ForumTitleChanged event,
     Emitter<CreateForumState> emit,
   ) {
-    emit(state.copyWith(page: event.page));
-    debugPrint(event.page.toString());
+    emit(state.copyWith(title: event.title));
+  }
+
+  void _forumIsAnonymousChanged(
+    ForumIsAnonymousChanged event,
+    Emitter<CreateForumState> emit,
+  ) {
+    emit(state.copyWith(is_anonymous: event.is_anonymous));
+  }
+
+  void _forumIsActiveChanged(
+    ForumIsActiveChanged event,
+    Emitter<CreateForumState> emit,
+  ) {
+    emit(state.copyWith(is_active: event.is_active));
+  }
+
+  void _forumThemeChanged(
+    ForumThemeChanged event,
+    Emitter<CreateForumState> emit,
+  ) {
+    emit(state.copyWith(theme: event.theme));
+  }
+
+  void _forumBodyChanged(
+    ForumBodyChanged event,
+    Emitter<CreateForumState> emit,
+  ) {
+    emit(state.copyWith(body: event.body));
+  }
+
+  Future<void> _forumOpenGallary(
+    ForumOpenGallary event,
+    Emitter<CreateForumState> emit,
+  ) async {
+    final permitted = await PhotoManager.requestPermissionExtend();
+    if (permitted.isAuth) {
+      emit(state.copyWith());
+      final albums = await PhotoManager.getAssetPathList(onlyAll: true);
+      final recentAlbum = albums.first;
+
+      // Now that we got the album, fetch all the assets it contains
+      final recentAssets = await recentAlbum.getAssetListRange(
+        start: 0, // start at index 0
+        end: 1000000, // end at a very big index (to get all the assets)
+      );
+      emit(
+        state.copyWith(
+          gallaryIsOpen: !state.gallaryIsOpen,
+          photosFromGallary: recentAssets,
+        ),
+      );
+    }
   }
 }
