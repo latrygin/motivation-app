ac76eac2e8a7bd416df5bb0c1f2b47781482c347
latrygin <latrygin@icloud.com>
2022-12-25 18:40:07 +0100

    Update wrapper.dart

--- lib/widgets/wrapper.dart
+++ lib/widgets/wrapper.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: use_named_constants
 
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:motivation/assets/themes/change_theme.dart';
@@ -26,39 +28,57 @@ class Wrapper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<ThemeModel>(
-      builder: (context, themeNotifier, children) {
-        return AnnotatedRegion<SystemUiOverlayStyle>(
-          value: SystemUiOverlayStyle(
-            //systemNavigationBarDividerColor: Colors.blue,
-            //systemNavigationBarColor: Colors.red,
-            systemNavigationBarIconBrightness: Theme.of(context).brightness,
-            statusBarIconBrightness: Theme.of(context).brightness,
-            statusBarBrightness: Theme.of(context).brightness,
-            //statusBarColor: Colors.green,
-          ),
-          child: Scaffold(
-            resizeToAvoidBottomInset: false,
-            appBar: appBar,
-            bottomNavigationBar: bottomNavigationBar,
-            body: Container(
-              color: safeAreaColor,
-              child: SafeArea(
-                bottom: bottom,
-                child: Container(
-                  width: double.infinity,
-                  height: double.infinity,
-                  color: backgroundColor == Colors.transparent
-                      ? Theme.of(context).scaffoldBackgroundColor
-                      : Theme.of(context).hintColor,
-                  padding: padding,
-                  child: child,
-                ),
-              ),
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle(
+        //systemNavigationBarDividerColor: Colors.blue,
+        //systemNavigationBarColor: Colors.red,
+        systemNavigationBarIconBrightness:
+            Theme.of(context).brightness == Brightness.dark
+                ? Platform.isIOS
+                    ? Brightness.dark
+                    : Brightness.light
+                : Platform.isAndroid
+                    ? Brightness.dark
+                    : Brightness.light,
+        statusBarIconBrightness: Theme.of(context).brightness == Brightness.dark
+            ? Platform.isIOS
+                ? Brightness.dark
+                : Brightness.light
+            : Platform.isAndroid
+                ? Brightness.dark
+                : Brightness.light,
+        statusBarBrightness: Theme.of(context).brightness == Brightness.dark
+            ? Platform.isIOS
+                ? Brightness.dark
+                : Brightness.light
+            : Platform.isAndroid
+                ? Brightness.dark
+                : Brightness.light,
+        statusBarColor: Colors.transparent,
+        // systemNavigationBarIconBrightness: Theme.of(context).brightness,
+        // statusBarIconBrightness: Theme.of(context).brightness,
+        // statusBarBrightness: Theme.of(context).brightness,
+      ),
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: appBar,
+        bottomNavigationBar: bottomNavigationBar,
+        body: Container(
+          color: safeAreaColor,
+          child: SafeArea(
+            bottom: bottom,
+            child: Container(
+              width: double.infinity,
+              height: double.infinity,
+              color: backgroundColor == Colors.transparent
+                  ? Theme.of(context).scaffoldBackgroundColor
+                  : Theme.of(context).hintColor,
+              padding: padding,
+              child: child,
             ),
           ),
-        );
-      },
+        ),
+      ),
     );
   }
 }
