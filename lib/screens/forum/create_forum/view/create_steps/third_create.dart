c2d1d206723901aed2c380109f202389856c81f7
latrygin <latrygin@icloud.com>
2022-12-25 19:48:34 +0100

    Update third_create.dart

--- lib/screens/forum/create_forum/view/create_steps/third_create.dart
+++ lib/screens/forum/create_forum/view/create_steps/third_create.dart
@@ -3,7 +3,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:motivation/screens/forum/create_forum/bloc/create_forum_bloc.dart';
-import 'package:provider/provider.dart';
 
 class ThirdForumPage extends StatelessWidget {
   const ThirdForumPage({super.key});
