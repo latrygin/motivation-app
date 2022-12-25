90be7ec003057c5b46055ab7c39be8a825a8757a
latrygin <latrygin@icloud.com>
2022-12-25 19:48:39 +0100

    Update first_create.dart

--- lib/screens/forum/create_forum/view/create_steps/first_create.dart
+++ lib/screens/forum/create_forum/view/create_steps/first_create.dart
@@ -2,7 +2,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:provider/provider.dart';
 
 import '../../bloc/create_forum_bloc.dart';
 
